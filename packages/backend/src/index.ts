a8f26eafb2bd321be09c9190d281690b6c8a499d
Mike Gajda <github@mikegajda.com>
2019-09-26 11:51:00 -0400

    Shared interfaces and lerna commands

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
+import { IUser } from '@fuse-starter-typescript/shared/interfaces'
 export const app = express()
 
 app.use(express.json())
 
 app.get('/api/test', function(req, res){
-    res.send({
-        "test": "response"
-    })
+
+    let user: IUser = {
+        firstName: "First",
+        lastName: "Last"
+    }
+    res.send(user)
 })
 
 
