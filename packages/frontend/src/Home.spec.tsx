4f2b932f36497d00fecb50ab72bd9bc37cf04b92
Mike Gajda <github@mikegajda.com>
2019-10-09 17:20:47 -0400

    Improve Home.spec.tsx

--- packages/frontend/src/Home.spec.tsx
+++ packages/frontend/src/Home.spec.tsx
@@ -9,7 +9,7 @@ import fetchMock = require('fetch-mock');
 
 
 describe('Home', () => {
-    it('can be shallow rendered', async () => {
+    it('Can renders a user when the backend is working', async () => {
         let mockUser: IUser = {
             firstName: "testFirst",
             lastName: "testLast"
@@ -22,6 +22,27 @@ describe('Home', () => {
         )
         // give the store a bit of time to receive the action and put it in the store
         await sleep(500);
-        expect(mockStore.getActions().length).toEqual(1)
+
+        // now we should have three actions in the store. 1. set_user_fetching, 2. add_alert
+        // 3. set_user_fetching
+        expect(mockStore.getActions().length).toEqual(3)
+
+    })
+
+    it('Can renders an alert when something goes wrong', async () => {
+        fetchMock.mock('/api/test', {status: 500})
+        mount(
+          <Provider store={mockStore}>
+              <Home/>
+          </Provider>
+        )
+        // give the store a bit of time to receive the action and put it in the store
+        await sleep(500);
+
+        // now we should have three actions in the store. 1. set_user_fetching, 2. add_alert
+        // 3. set_user_fetching
+        // todo: some work on checking the actual actions
+        expect(mockStore.getActions().length).toEqual(3)
+
     })
 })
