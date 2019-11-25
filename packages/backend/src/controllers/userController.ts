f75241d19a258f4b632929a09a685f131b563e1f
Mike Gajda <github@mikegajda.com>
2019-11-25 17:33:37 -0500

    Break user controller to its own file to resolve #8

---
+++ packages/backend/src/controllers/userController.ts
@@ -0,0 +1,25 @@
+import { getDatabase } from '../mongo'
+import { IUser } from '@fuse-starter-typescript/shared/interfaces/IUser'
+import { User } from '@fuse-starter-typescript/shared/models/User'
+import { Request, Response } from 'express'
+
+export async function getUsers(req: Request, res: Response) {
+  // ensure we have a DB connection, and seed the database
+  // todo: move seeding to another place, but for now
+  let db = await getDatabase()
+  const user: IUser = new User('Mike', 'Gajda')
+  await db.add(User, user)
+
+  // now get all the users
+  let users: User[] = await db.find(User)
+  res.send(users)
+}
+
+export async function getUser(req: Request, res: Response) {
+  console.log("received request to /api/user/", req.params.uuid)
+  // ensure we have a DB connection, and seed the database
+  let db = await getDatabase()
+  // now get all the users
+  let user = await db.get(User, {uuid: req.params.uuid})
+  res.send(user)
+}
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,37 +1,15 @@
 import express from 'express'
 import 'reflect-metadata'
-import { IUser } from '@fuse-starter-typescript/shared/interfaces/IUser'
-import { User } from '@fuse-starter-typescript/shared/models/User'
-import { getDatabase, shutdownMongo } from './mongo'
-import { Database } from '@marcj/marshal-mongo'
+import { shutdownMongo } from './mongo'
+import { getUser, getUsers } from './controllers/userController'
 
 export const app = express()
 
 app.use(express.json())
 
-let db: Database
+app.get('/api/users', getUsers)
 
-app.get('/api/users', async function (req, res) {
-  // ensure we have a DB connection, and seed the database
-  // todo: move seeding to another place, but for now
-  db = await getDatabase()
-  const user: IUser = new User('Mike', 'Gajda')
-  await db.add(User, user)
-
-  // now get all the users
-  let users: User[] = await db.find(User)
-  res.send(users)
-})
-
-app.get('/api/user/:uuid', async function (req, res) {
-  console.log("received request to /api/user/", req.params.uuid)
-  // ensure we have a DB connection, and seed the database
-  db = await getDatabase()
-
-  // now get all the users
-  let user = await db.get(User, {uuid: req.params.uuid})
-  res.send(user)
-})
+app.get('/api/user/:uuid', getUser)
 
 app.listen(3000, async function () {
   console.log('Listening on port 3000')
@@ -41,4 +19,3 @@ process.on('exit', async function () {
   console.log('index.ts received exit event')
   await shutdownMongo()
 })
-
