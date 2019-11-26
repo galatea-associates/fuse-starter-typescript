294aa259fb64c909c4b7ee9719bdc5a3c2751596
Mike Gajda <github@mikegajda.com>
2019-11-26 10:58:34 -0500

    Move to async tests to avoid promise resolution via done()

--- packages/backend/tests/index.ts
+++ packages/backend/tests/index.ts
@@ -4,24 +4,24 @@ import { IUser } from '@fuse-starter-typescript/shared/interfaces/IUser'
 import chai = require('chai')
 import chaiHttp = require('chai-http')
 import { shutdownMongo } from '../src/mongo'
+const doc = require('test2doc')
 
 chai.use(chaiHttp)
 
 describe('index.ts', function () {
-  it('should return a User when requested', function (done) {
-    chai.request(app).get('/api/users').end((err, res) => {
-      const user: IUser = {
-        firstName: 'Mike',
-        lastName: 'Gajda'
-      }
-      expect(res).to.have.status(200)
-      expect(res).to.be.json
-      expect(res.body[0].firstName).to.equal(user.firstName)
-      expect(res.body[0].lastName).to.equal(user.lastName)
-      done()
-    })
+  it('should return a User when requested', async function () {
+    let res = await chai.request(app).get('/api/users')
+    const user: IUser = {
+      firstName: 'Mike',
+      lastName: 'Gajda'
+    }
+    expect(res).to.have.status(200)
+    expect(res).to.be.json
+    expect(res.body[0].firstName).to.equal(user.firstName)
+    expect(res.body[0].lastName).to.equal(user.lastName)
   })
   after(async function() {
     await shutdownMongo()
+    doc.emit('api-documentation.yaml', 'swagger')
   })
 })
