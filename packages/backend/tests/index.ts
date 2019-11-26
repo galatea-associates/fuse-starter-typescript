6486c4000163d4f8758e07c397d4a0973f98354f
Mike Gajda <github@mikegajda.com>
2019-11-26 17:06:34 -0500

    Add working swagger tests. Add dependency on symbol-description set at 1.0.0 to get around errors with test2doc doc.resBody() functionality. Test2doc is incompatible with v1.1.0 of symbol-description

--- packages/backend/tests/index.ts
+++ packages/backend/tests/index.ts
@@ -15,11 +15,9 @@ describe('controllers/userController.ts', async function () {
     requester = chai.request(app).keepOpen()
   })
   doc.group('Users').desc('Users controller').is((doc:any) => {
-    doc.action('Get all users').is(async function (doc:any) {
-      it('should return a User when requested', async function () {
-        console.log("I'm in here")
+    doc.action('Get all Users').is(async function (doc:any) {
+      it('should return a list of Users when requested', async function () {
         const res = await requester.get(doc.get('/api/users'))
-        console.log(res.body)
         const user: IUser = {
           firstName: 'Mike',
           lastName: 'Gajda'
@@ -28,10 +26,8 @@ describe('controllers/userController.ts', async function () {
         expect(res).to.be.json
         expect(res.body[0].firstName).to.equal(user.firstName)
         expect(res.body[0].lastName).to.equal(user.lastName)
-        let body = doc.resBody(res.body)
-        console.log("body", body)
+        doc.resBody(res.body)
 
-        //body[0].firstName.desc('User first name')
       })
     })
   })
@@ -41,33 +37,3 @@ describe('controllers/userController.ts', async function () {
     doc.emit('api-documentation.yaml', 'swagger')
   })
 })
-
-// doc.group('#Users').is((doc:any) => {
-//   describe('index.ts', function () {
-//     doc.action('Get all users').is((doc:any) => {
-//       it('should return a User when requested', function(done:any) {
-//         chai.request(app).get(doc.get('/api/users')).end((err, res)=> {
-//           console.log("body", res.body)
-//
-//           const user: IUser = {
-//             firstName: 'Mike',
-//             lastName: 'Gajda'
-//           }
-//           expect(res).to.have.status(200)
-//           expect(res).to.be.json
-//           expect(res.body[0].firstName).to.equal(user.firstName)
-//           expect(res.body[0].lastName).to.equal(user.lastName)
-//           doc.resBody(res.body.toString())
-//           done()
-//         })
-//
-//       })
-//     })
-//
-//     after(async function() {
-//       await shutdownMongo()
-//       doc.emit('api-documentation.yaml', 'swagger')
-//     })
-//   })
-// })
-
