8b2f4d1751bed179c3fd57ec34cafcfac321a97c
Michael Gajda <github@mikegajda.com>
2019-12-03 16:11:06 -0500

    Comment out swagger to generate a different function

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -14,15 +14,15 @@ console.log(process.version);
 
 app.use(express.json())
 
-try{
-  const swaggerUi = require('swagger-ui-express')
-  const YAML = require('yamljs')
-  const swaggerDocument = YAML.load('./api-documentation.yaml')
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-}
-catch {
-  console.log("something went wrong with loading documentation, but will not fail")
-}
+// try{
+//   const swaggerUi = require('swagger-ui-express')
+//   const YAML = require('yamljs')
+//   const swaggerDocument = YAML.load('./api-documentation.yaml')
+//   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+// }
+// catch {
+//   console.log("something went wrong with loading documentation, but will not fail")
+// }
 
 // this async wrapper allows us to make an async call, then catch any async errors with the .catch() block
 // this will handle any "Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 3)"
